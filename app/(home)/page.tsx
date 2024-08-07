ada74e08bec316f3a2f6de05883375d57bb738f9
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-08-07 20:17:35 +0530

    Complete the Header Component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,11 @@
+import { Header } from "@/components/header/header";
+import GridPattern from "@/components/magicui/grid-pattern";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
 export default function Home() {
   return (
     <main>
-      
+      <Header/>
     </main>
   );
 }
---
+++ components/header/mobile-navbar.tsx
@@ -0,0 +1,21 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger
+  } from "@/components/ui/sheet";
+
+  import { Menu } from "lucide-react";
+import Sidebar from "./sidebar";
+
+  export const MobileNavbar = () => {
+    return (
+      <Sheet>
+        <SheetTrigger className="lg:hidden pr-4 hover:opacity-75 transition">
+          <Menu className="text-white"/>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-5">
+          <Sidebar />
+        </SheetContent>
+      </Sheet>
+    )
+  }
